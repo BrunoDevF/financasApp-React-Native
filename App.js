ad7bf5202ea35c31008e4decd95dc5665306d085
Bruno Henrique <brunohenriquex00@gmail.com>
2021-04-06 14:21:54 -0300

    usando statusbar

--- App.js
+++ App.js
@@ -2,12 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Text, View } from 'react-native';
 
-import { Container, Titulo } from './src/styles';
+/* esse getStatusBarHeight vai nos ajudar a calcular a altura da nossa view */
+// import { getStatusBarHeight } from 'react-native-status-bar-height';
 
+import { Container, Titulo, Nome, BotaoNormal, BotoaoSimples, StatusBar } from './src/styles';
+/*************************
+ *  Instalar a lib react-native-status-bar-height
+ *  para corrigir a altura da status bar com todos os smartphones
+ *************************/
 export default function App() {
   return (
     <Container>
+      <StatusBar backgroundColor="transparent" barStyle="light-content"
+        translucent={true}
+      />
+
       <Titulo>Ola mundo react</Titulo>
+      <Nome>Bruno Henrique </Nome>
+      <BotaoNormal>
+        <BotoaoSimples>Entrar</BotoaoSimples>
+      </BotaoNormal>
     </Container>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,14 +1,35 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+
+
 
 export const Container = styled.View `
   flex:1;
   justify-content: center;
   align-items: center;
-  background-color: #1f4358;
+  background-color: #121212;
+  padding-top: ${ 0 + getStatusBarHeight() };
 `;
 export const Titulo = styled.Text `
   font-size: 25px;
-  color: #8532b6;
+  color: #fff;
+`;
+export const Nome = styled.Text`
+    font-size:45;
+    color: #fff;
+`;
+export const BotaoNormal = styled.TouchableOpacity`
+    background-color: #121212;
+    border: 2px solid #fff;
+    border-radius: 10px; 
+    padding: 10px;
+    width: 90%;
+    justify-content: center;
+    align-items: center;
+`;
+export const BotoaoSimples = styled.Text`
+    font-size: 20;
+    color: #fff;
 `;
 
 
