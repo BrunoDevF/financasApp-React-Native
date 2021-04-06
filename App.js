27994015815bc4813953eab576efa2ad66a78257
Bruno Henrique <brunohenriquex00@gmail.com>
2021-04-06 15:33:08 -0300

    instalando firebase e async storage

--- App.js
+++ App.js
@@ -1,27 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import {Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-/* esse getStatusBarHeight vai nos ajudar a calcular a altura da nossa view */
-// import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { Container } from './src/styles';
+import firebase from './src/services/firebaseConnection';
 
-import { Container, Titulo, Nome, BotaoNormal, BotoaoSimples, StatusBar } from './src/styles';
-/*************************
- *  Instalar a lib react-native-status-bar-height
- *  para corrigir a altura da status bar com todos os smartphones
- *************************/
+
+
 export default function App() {
   return (
     <Container>
-      <StatusBar backgroundColor="transparent" barStyle="light-content"
-        translucent={true}
-      />
-
-      <Titulo>Ola mundo react</Titulo>
-      <Nome>Bruno Henrique </Nome>
-      <BotaoNormal>
-        <BotoaoSimples>Entrar</BotoaoSimples>
-      </BotaoNormal>
+      
     </Container>
   );
 }
