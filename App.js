b986a030b800b80d51ca982060b559c9c18e4fb9
Bruno Henrique <brunohenriquex00@gmail.com>
2021-04-06 12:20:12 -0300

    iniciando trabalhos e instalando Styled-Component ao projeto

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
+
+import { Container, Titulo } from './src/styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Titulo>Ola mundo react</Titulo>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View `
+  flex:1;
+  justify-content: center;
+  align-items: center;
+  background-color: #1f4358;
+`;
+export const Titulo = styled.Text `
+  font-size: 25px;
+  color: #8532b6;
+`;
+
+
+
+
+
+
+
